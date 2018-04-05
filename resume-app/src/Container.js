a7168a577f4dcc76966a0400effbf5adaa5d51bc
umworksite <umworksite@gmail.com>
2018-04-06 03:15:46 +0800

    add google maps

--- resume-app/src/Container.js
+++ resume-app/src/Container.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap_grid.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-class Container extends Component {
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 22.627278, lng: 120.301435 }}
+  >
+    <Marker
+      position={{ lat: 22.627278, lng: 120.301435 }}
+    />
+  </GoogleMap>
+));
+
+class Container extends React.Component {
+  
   render() {
     return (
     	<div  className="container">
@@ -21,6 +40,14 @@ class Container extends Component {
 					<p>姓名：林郁翔</p>
 					<p>興趣：看海、設計、畫圖、思考、旅遊</p>
 					<p>專長：版面設計、網頁前端、網站架設、WordPress版型設計、資料呈現</p>
+					
+					<MapWithAMarker
+					  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyApV3ZDA2YrPip7taiHIb-2k72QSmG9egg&v=3.exp&libraries=geometry,drawing,places"
+					  loadingElement={<div style={{ height: `100%` }} />}
+					  containerElement={<div style={{ height: `300px` }} />}
+					  mapElement={<div style={{ height: `100%` }} />}
+					/>
+					<br/>
 					<br/>
 					</div>
 				</div>
